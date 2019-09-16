01c6a4f8ae630a52ea7391ba0438706373b7cece
Mrpsousa <urameshi.uba@gmail.com>
2019-09-16 20:53:17 -0300

    Cards

--- project/resources/views/index.blade.php
+++ project/resources/views/index.blade.php
@@ -106,7 +106,7 @@
     <!-- Scroll Spy -->
 <div class="container">
     <div class="row">
-        <div class="col-12 text-center my-4">
+        <div class="col-12 text-center mt-4">
             <h1  class="display-2">Pesca Web!</h1>
             <h3>Portal de Apoio aos alunos de Ciência   da Computação da UESC</h3>
         </div>
@@ -127,10 +127,194 @@
         </div>
 
         <div class="col-9">
-
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title"> Objetivo</h4>
+                        <h6 class="card-subtitle mb-2 text-muted">Título motivacional</h6>
+                        <p class="card-text">Explicação blá blá blá blá blá blá blá blá blá blá</p>
+                        <button class="btn btn-dark" href="#" > Algo? </button>
+                    </div>
+                </div>
         </div>
     </div>
+
+    <!-- Cards -->
+<div class="row mb-5">
+    <div class="col-sm-3">
+    </div>
+    <div class="col-sm-3">
+        <div class="card">
+            <img class="card-img-top" src="{{ asset('img/sem01.jpg') }}">
+            <div class="card-body">
+                <h4 class="card-title"> 1º Semestre</h4>
+                <h6 class="card-subtitle mb-2 text-muted">Matérias do 1º regular</h6>
+                </div>
+                    <uL class="list-group list-group-flush">
+                        <li class="list-group-item"> Lp1  cal1 ld1 </li>
+                        <li class="list-group-item"> cal1 ingles </li>
+                        <li class="list-group-item"> metodo adb </li>
+                    </uL>
+                <div class="card-body">
+                <button class="btn btn-dark" href="#" > Acesso </button>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-3">
+            <div class="card">
+                <img class="card-img-top" src="{{ asset('img/sem01.jpg') }}">
+                <div class="card-body">
+                    <h4 class="card-title"> 2º Semestre</h4>
+                    <h6 class="card-subtitle mb-2 text-muted">Matérias do 2º regular</h6>
+                    </div>
+                        <uL class="list-group list-group-flush">
+                            <li class="list-group-item"> Lp1  cal1 ld1 </li>
+                            <li class="list-group-item"> cal1 ingles </li>
+                            <li class="list-group-item"> metodo adb </li>
+                        </uL>
+                    <div class="card-body">
+                    <button class="btn btn-dark" href="#" > Acesso </button>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-3">
+            <div class="card">
+                <img class="card-img-top" src="{{ asset('img/sem01.jpg') }}">
+                <div class="card-body">
+                    <h4 class="card-title"> 3º Semestre</h4>
+                    <h6 class="card-subtitle mb-2 text-muted">Matérias do 3º regular</h6>
+                </div>
+                    <uL class="list-group list-group-flush">
+                            <li class="list-group-item"> Lp1  cal1 ld1 </li>
+                            <li class="list-group-item"> cal1 ingles </li>
+                            <li class="list-group-item"> metodo adb </li>
+                        </uL>
+                    <div class="card-body">
+                    <button class="btn btn-dark" href="#" > Acesso </button>
+                </div>
+            </div>
+    </div>
 </div>
+
+<!-- Card 2-->
+<div class="row mb-5">
+    <div class="col-sm-3">
+        </div>
+        <div class="col-sm-3">
+            <div class="card">
+                <img class="card-img-top" src="{{ asset('img/sem01.jpg') }}">
+                <div class="card-body">
+                    <h4 class="card-title"> 4º Semestre</h4>
+                    <h6 class="card-subtitle mb-2 text-muted">Matérias do 4º regular</h6>
+                </div>
+                        <uL class="list-group list-group-flush">
+                            <li class="list-group-item"> Lp1  cal1 ld1 </li>
+                            <li class="list-group-item"> cal1 ingles </li>
+                            <li class="list-group-item"> metodo adb </li>
+                        </uL>
+                    <div class="card-body">
+                    <button class="btn btn-dark" href="#" > Acesso </button>
+                    </div>
+            </div>
+    </div>
+
+    <div class="col-sm-3">
+        <div class="card">
+                <img class="card-img-top" src="{{ asset('img/sem01.jpg') }}">
+                <div class="card-body">
+                        <h4 class="card-title"> 5º Semestre</h4>
+                        <h6 class="card-subtitle mb-2 text-muted">Matérias do 5º regular</h6>
+                        </div>
+                            <uL class="list-group list-group-flush">
+                                <li class="list-group-item"> Lp1  cal1 ld1 </li>
+                                <li class="list-group-item"> cal1 ingles </li>
+                                <li class="list-group-item"> metodo adb </li>
+                            </uL>
+                        <div class="card-body">
+                        <button class="btn btn-dark" href="#" > Acesso </button>
+                        </div>
+                </div>
+        </div>
+    <div class="col-sm-3">
+            <div class="card">
+                    <img class="card-img-top" src="{{ asset('img/sem01.jpg') }}">
+                    <div class="card-body">
+                        <h4 class="card-title"> 6º Semestre</h4>
+                        <h6 class="card-subtitle mb-2 text-muted">Matérias do 6º regular</h6>
+                        </div>
+                            <uL class="list-group list-group-flush">
+                                <li class="list-group-item"> Lp1  cal1 ld1 </li>
+                                <li class="list-group-item"> cal1 ingles </li>
+                                <li class="list-group-item"> metodo adb </li>
+                            </uL>
+                        <div class="card-body">
+                            <button class="btn btn-dark" href="#" > Acesso </button>
+                        </div>
+                    </div>
+            </div>
+    </div>
+
+<!-- Cards 3 -->
+<div class="container">
+        <div class="row mb-5">
+                <div class="col-sm-3">
+                </div>
+                <div class="col-sm-3">
+                    <div class="card">
+                        <img class="card-img-top" src="{{ asset('img/sem01.jpg') }}">
+                        <div class="card-body">
+                                <h4 class="card-title"> 7º Semestre</h4>
+                                <h6 class="card-subtitle mb-2 text-muted">Matérias do 7º regular</h6>
+                            </div>
+                                <uL class="list-group list-group-flush">
+                                    <li class="list-group-item"> Lp1  cal1 ld1 </li>
+                                    <li class="list-group-item"> cal1 ingles </li>
+                                    <li class="list-group-item"> metodo adb </li>
+                                </uL>
+                            <div class="card-body">
+                            <button class="btn btn-dark" href="#" > Acesso </button>
+                            </div>
+                </div>
+        </div>
+
+                <div class="col-sm-3">
+                        <div class="card">
+                                <img class="card-img-top" src="{{ asset('img/sem01.jpg') }}">
+                                <div class="card-body">
+                                    <h4 class="card-title"> 8º Semestre</h4>
+                                    <h6 class="card-subtitle mb-2 text-muted">Matérias do 8º regular</h6>
+                                    </div>
+                                    <uL class="list-group list-group-flush">
+                                        <li class="list-group-item"> Lp1  cal1 ld1 </li>
+                                        <li class="list-group-item"> cal1 ingles </li>
+                                        <li class="list-group-item"> metodo adb </li>
+                                    </uL>
+                                    <div class="card-body">
+                                    <button class="btn btn-dark" href="#" > Acesso </button>
+                                    </div>
+                            </div>
+                </div>
+        <div class="col-sm-3">
+                <div class="card">
+                    <img class="card-img-top" src="{{ asset('img/sem01.jpg') }}">
+                    <div class="card-body">
+                                <h4 class="card-title"> Optativas</h4>
+                                <h6 class="card-subtitle mb-2 text-muted">Matérias Optativas</h6>
+                                </div>
+                                    <uL class="list-group list-group-flush">
+                                        <li class="list-group-item"> Lp1  cal1 ld1 </li>
+                                        <li class="list-group-item"> cal1 ingles </li>
+                                        <li class="list-group-item"> metodo adb </li>
+                                    </uL>
+                                <div class="card-body">
+                                <button class="btn btn-dark" href="#" > Acesso </button>
+                                </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
